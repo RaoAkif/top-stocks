bef5402ab2766030e09b1310e382dfefee5a199d
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2025-02-11 18:19:48 +0500

    Update apiConfig.ts

--- app/config/apiConfig.ts
+++ app/config/apiConfig.ts
@@ -1,8 +1,5 @@
 export const API_CONFIG = {
-    EMBEDDING_URL: "https://txt-embd.onrender.com/get-embedding/",
-    PINECONE_URL: "https://nyse-vahacxe.svc.aped-4627-b74a.pinecone.io/query",
-    PINECONE_API_KEY:
-      process.env.PINECONE_API_KEY ||
-      "pcsk_5GQWiD_Kuesb5xDFAjcWqMsge1kJEiNwTurCzq1DxcBWWtvVfjS44wJabEHyx9rce9CAtn",
-  };
-  
+  EMBEDDING_URL: process.env.EMBEDDING_URL,
+  PINECONE_URL: process.env.PINECONE_URL,
+  PINECONE_API_KEY: process.env.PINECONE_API_KEY,
+};
